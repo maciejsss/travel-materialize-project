ae4ceccbe132493a763418dee8ea337b3087d965
Maciej Świątek <mswiatek0@gmail.com>
2019-05-11 14:55:55 +0200

    add slider script

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+// SLIDER
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+})
